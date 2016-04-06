2e6a1395756b526c3bbb9c271b7f5f88d0abdd3d
moltrex <iluca.silviu@gmail.com>
2016-04-06 15:28:02 +0300

    adding style to sidebar head list

--- include/view/sidebar/view-sidebar-header.php
+++ include/view/sidebar/view-sidebar-header.php
@@ -1,9 +1,15 @@
 <div class="nr sidebar-head">
     <ul class="nav nav-tabs" role="tablist">
-        <li role="presentation" class="sidebar-head-category active"><a href="#sidebar-category-wrap" aria-controls="sidebar-category-wrap" role="tab" data-toggle="tab"><i class="fa fa-archive"></i></a></li>
-        <li class="sidebar-head-filter" role="presentation"><a href="#sidebar-filters-wrap" aria-controls="sidebar-filters-wrap" role="tab" data-toggle="tab"><i class="fa fa-tasks"></i></a></li>
-        <!--
-        <li role="presentation"><a href="#sidebar-tags-wrap" aria-controls="sidebar-tags-wrap" role="tab" data-toggle="tab"><i class="fa fa-tags"></i></a></li>
-        -->
+        <li role="presentation" class="sidebar-head-item sidebar-head-category active">
+            <a href="#sidebar-category-wrap" aria-controls="sidebar-category-wrap" role="tab" data-toggle="tab">
+                <i class="fa fa-archive"></i>
+            </a>
+        </li>
+        <li class="sidebar-head-item sidebar-head-filter" role="presentation">
+            <a href="#sidebar-filters-wrap" aria-controls="sidebar-filters-wrap" role="tab" data-toggle="tab">
+                <i class="fa fa-tasks"></i>
+            </a>
+        </li>
+        <!--<li role="presentation"><a href="#sidebar-tags-wrap" aria-controls="sidebar-tags-wrap" role="tab" data-toggle="tab"><i class="fa fa-tags"></i></a></li>-->
     </ul>
 </div>
